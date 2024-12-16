b245374eb19d23642b454ce9be61cd8c65e36c19
Хандыго Артем <handik07@mail.ru>
2024-12-16 11:11:23 +0300

    Lab03

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,73 @@
-//==============================================================================
-///
-/// @file
-/// @brief Программа для робота Карела и стартовая функция
-///
-//==============================================================================
-
-
 #include "karel.h"
+#include <iostream>
 
+
+void PickAllBeepers();
+void TurnRight();
+void PutDoubleBeepers(int& beeper_cell);
+void PickAllBeepers(int& beeper_cell);
+void GrapAndPut(int& beeper_cell);
 
 //==============================================================================
 /// Программа для робота Карела
 ///
 void KarelProgram() {
 /// @todo Добавить программу для Карела
+ bool is_even = false;
+    int beeper_cell = 0;
+    while(FrontIsClear()){
+        while(FrontIsClear()){
+            GrapAndPut(beeper_cell);
+            Move();
+        }
+        GrapAndPut(beeper_cell);
+        if(!is_even){
+            TurnLeft();
+            if(!FrontIsClear())
+                return;
+            Move();
+            TurnLeft();
+
+        }
+        else{
+            TurnRight();
+            if(!FrontIsClear())
+                return;
+            Move();
+            TurnRight();
+        }
+        is_even = !is_even;
+        }
 
 }
 
+void PutDoubleBeepers(int& beeper_cell){
+    for( ;beeper_cell > 0; beeper_cell--){
+        for(int i = 0; i < 2; i++)
+            PutBeeper();
+    }
 
-//==============================================================================
-/// Стартовая функция
-///
+}
+
+void PickAllBeepers(int& beeper_cell){
+    while (BeepersPresent()){
+        PickBeeper();
+        beeper_cell++;
+    }
+}
+
+void TurnRight(){
+    for(int i = 0; i < 3; i++){
+        TurnLeft();
+    }
+}
+
+void GrapAndPut(int& beeper_cell){
+    PickAllBeepers(beeper_cell);
+    PutDoubleBeepers(beeper_cell);
+}
 int main() {
-    LoadWorld("worlds/.w");
+    LoadWorld("worlds/Double_beepers_01.w");
     KarelProgram();
     Finish();
     return 0;
